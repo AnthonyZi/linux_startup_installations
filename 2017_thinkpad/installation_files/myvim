f2a4b86a1467f5e801e59077886ce6c4638cc461
AnthonyZi <AnthonyZi@users.noreply.github.com>
2017-05-24 03:20:24 +0200

    2017-commit-thinkpadt430

---
+++ 2017_thinkpad/installation_files/myvim
@@ -0,0 +1,54 @@
+set number
+set linebreak
+set showbreak=+++
+set showmatch
+set visualbell
+
+set hlsearch
+set smartcase
+set ignorecase
+set incsearch
+
+set autoindent
+"set tabstop=8
+"set softtabstop=4
+"set shiftwidth=4
+set expandtab
+set smartindent
+set smarttab
+
+set ruler
+
+set undolevels=1000
+
+"Cursor tends to rest in the middle-area of screen
+set scrolloff=10
+
+"80th line is red
+set formatoptions=qrn1
+set colorcolumn=80
+
+"remap for ESC
+ino jkl <esc>
+ino jlk <esc>
+ino kjl <esc>
+ino klj <esc>
+ino ljk <esc>
+ino lkj <esc>
+nno jkl <esc>
+nno jlk <esc>
+nno kjl <esc>
+nno klj <esc>
+nno ljk <esc>
+nno lkj <esc>
+vno jkl <esc>
+vno jlk <esc>
+vno kjl <esc>
+vno klj <esc>
+vno ljk <esc>
+vno lkj <esc>
+
+"syntax settings
+syntax on
+autocmd FileType c set cindent
+"set cindent
---
+++ 2017_thinkpad/installations.sh
@@ -0,0 +1,118 @@
+# ################# Manually #############
+# #### INSTALL SUDO
+# #echo " [exec] su root"
+# #su root
+# #following can be done automatically
+# echo " [info] automatically adjust /etc/apt/sources.list for debian jessie"
+# echo " [exec] cat sources_debian_jessie > /etc/apt/sources.list"
+# cat installation_files/sources_debian_jessie > /etc/apt/sources.list
+# echo " [exec] apt-get update"
+# apt-get update
+# echo " [exec] apt-get upgrade"
+# apt-get upgrade
+# echo " [exec] apt-get install sudo"
+# apt-get install sudo
+# echo " [exec] apt-get install usermod"
+# apt-get install usermod
+# echo ""
+# echo " [info] exec: usermod -aG sudo [username] to add [username] to sudoers-group"
+# exit
+
+
+
+# ################# Automatically ##############
+# 
+# #### INSTALL STANDARD-PROGRAMMS
+# echo "Install standard-programms"
+# echo " [exec] sudo apt-get install vim"
+# sudo apt-get install vim
+# echo " [exec] sudo apt-get install gcc"
+# sudo apt-get install gcc
+# echo " [exec] sudo apt-get install g++"
+# sudo apt-get install g++
+# echo " [exec] sudo apt-get install tree"
+# sudo apt-get install tree
+# echo " [exec] sudo apt-get install git"
+# sudo apt-get install git
+# echo " [exec] cp installation_files/myvim ~/.vimrc"
+# cp installation_files/myvim ~/.vimrc
+# echo " [exec] sudo apt-get install evince"
+# sudo apt-get install evince
+# echo " [exec] sudo apt-get install gpicview"
+# sudo apt-get install gpicview
+# echo " [exec] sudo apt-get install zip"
+# sudo apt-get install zip
+# echo " [exec] sudo apt-get install gimp"
+# sudo apt-get install gimp
+# echo " [exec] sudo apt-get install audacity"
+# sudo apt-get install audacity
+# echo " [exec] sudo apt-get install iceweasel"
+# sudo apt-get install iceweasel
+# echo " [info] install following manually:"
+# echo " [info] "
+# echo " [info] ADDON : Adblock Plus"
+# echo " [info] ADDON : Firebug"
+# echo " [info] ADDON : NoScript"
+# echo " [info] ADDON : VimFx"
+# echo " [info] "
+# echo " [info] install adobe flash player manually"
+# echo " [info] "
+# echo " [info] make "startpage" your standard search-engine!"
+
+# 
+# 
+# #### INSTALL WLAN-interface
+# echo " [exec] sudo apt-get insall wicd wicd-curses"
+# sudo apt-get install wicd wicd-curses
+# echo " [exec] sudo cp installation_files/peap-mschapv2 /etc/wicd/encryption/templates"
+# sudo cp installation_files/peap-mschapv2 /etc/wicd/encryption/templates
+# echo " [exec] sudo sh -c 'echo "peap-mschapv2" >> /etc/wicd/encryption/templates/active'"
+# sudo sh -c "echo "peap-mschapv2" >> /etc/wicd/encryption/templates/active"
+
+
+# #### INSTALL GUI --- http://erikdubois.be/how-to-install-i3-on-arch-linux/
+# echo "Install xorg"
+# echo " [exec] sudo apt-get install xorg"
+# sudo apt-get install xorg
+# #xorg-server xorg-apps xorg-xinit xorg-twm xterm --noconfirm --needed
+# echo ""
+# echo "Install xterm"
+# echo " [exec] sudo apt-get install xterm"
+# sudo apt-get install xterm
+# echo ""
+# echo "Install graphics-driver"
+# # For Intel-graphics-cards
+# echo " [exec] sudo apt-get install xf86-video-intel"
+# #sudo apt-get install xf86-video-intel
+# #xf86-video-intel --noconfirm --needed
+# # In a virtual box
+# echo " [exec] sudo apt-get install virtualbox-guest-utils"
+# sudo apt-get install virtualbox-guest-utils
+# echo " [exec] sudo apt-get install slim"
+# sudo apt-get install slim 
+# echo " [exec] sudo apt-get install i3"
+# sudo apt-get install i3
+# echo " [exec] sudo apt-get install libnotify-bin"
+# sudo apt-get install libnotify-bin
+# echo " [exec] cp installation_files/myxsession ~/.xsession"
+# cp installation_files/myxsession ~/.xsession
+
+
+# #### INSTALL FINGERPRINT SENSOR - http://www.thinkwiki.org/wiki/How_to_enable_integrated_fingerprint_reader_with_fprint
+# echo ""
+# echo ""
+# echo "INSTALL FINGERPRINT SENSOR"
+# echo " [exec] sudo add-apt-repository ppa:fingerprint/fprint"
+# sudo add-apt-repository ppa:fingerprint/fprint
+# echo " [exec] sudo apt-get install libpam-fprintd"
+# sudo apt-get install libpam-fprintd
+# echo ""
+# echo " [info] The needed lines in common-auth should now be present:"
+# echo " [info] exec: grep fprint /etc/pam.d/common-auth"
+# echo " [info] ? -> auth	[succes=2 default=ignore] pam_fprintd.so"
+# echo ""
+# echo " [info] To configure your fingerprint:"
+# echo " [info] exec: fprintd-enroll"
+# echo ""
+# echo " [info] To eliminate fingerprint authentication from the display manager login do the followeing"
+# echo " [info] exec: sudo cp /etc/pam.d/common-auth /etc/pam.d/common-auth-nofinger"
