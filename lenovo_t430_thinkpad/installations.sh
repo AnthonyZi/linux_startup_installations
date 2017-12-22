26b9b3ded729d0d5d8d711d46278c4fae9c405d6
AnthonyZi_T430 <AnthonyZi@users.noreply.github.com>
2017-12-22 22:38:31 +0100

    edited first part of the script, which needs to be executed as root

--- lenovo_t430_thinkpad/installations.sh
+++ lenovo_t430_thinkpad/installations.sh
@@ -1,22 +1,28 @@
-# ################# Manually #############
-# #### INSTALL SUDO
-# #echo " [exec] su root"
-# #su root
-# #following can be done automatically
-# echo " [info] automatically adjust /etc/apt/sources.list for debian jessie"
-# echo " [exec] cat sources_debian_jessie > /etc/apt/sources.list"
-# #cat installation_files/sources_debian_jessie > /etc/apt/sources.list
-# echo " [exec] apt-get update"
-# apt-get update
-# echo " [exec] apt-get upgrade"
-# apt-get upgrade
-# echo " [exec] apt-get install sudo"
-# apt-get install sudo
-# echo " [exec] apt-get install usermod"
-# apt-get install usermod
-# echo ""
-# echo " [info] exec: usermod -aG sudo [username] to add [username] to sudoers-group"
-# exit
+################# Manually #############
+#### INSTALL SUDO
+#echo " [exec] su root"
+#su root
+#following can be done automatically
+echo " [info] adjust /etc/apt/sources.list for debian jessie like this:
+read tmp
+cat installation_files/sources_debian_jessie
+read tmp
+echo " [exec] apt-get update"
+read tmp
+apt-get update
+echo " [exec] apt-get upgrade"
+read tmp
+apt-get upgrade
+echo " [exec] apt-get install sudo"
+read tmp
+apt-get install sudo
+echo " [exec] apt-get install usermod"
+read tmp
+apt-get install usermod
+echo ""
+echo " [info] exec: usermod -aG sudo [username] to add [username] to sudoers-group"
+read tmp
+exit
 
 
 
